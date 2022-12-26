88a4db44b6d3347fd47c0a74016920429713e02b
Vaidehi-Kokare <vaidehikokare@gmail.com>
2022-12-26 18:35:16 +0530

    Added:Input of valid first name

---
+++ src/main/java/com/bridgelabz/FirstName.java
@@ -0,0 +1,23 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class FirstName {
+    static Pattern pattern = Pattern.compile("[A-Z]{3,}$");
+
+    public static void main(String[] args) { 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first name: ");
+        String firstName = scanner.next();
+        Matcher matcher = pattern.matcher(firstName);
+
+        if (matcher.matches()) {
+            System.out.println("matches");
+
+        } else {
+            System.out.println("not matching");
+        }
+    }
+}
